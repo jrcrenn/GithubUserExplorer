7e2d7ffb674a44d2cd2af6e8cba2459eb8c878d8
Jean-Robert Crenn <jrcrenn@MacBook-Pro-de-Jean-Robert.local>
2023-09-21 00:54:24 +0200

    refacto FizzBuzz

--- Algo/fizzbuzz.js
+++ Algo/fizzbuzz.js
@@ -1,18 +1,13 @@
 function fizzBuzz(n) {
-    for (let i = 1; i <= n; i++) {
-      let output = "";
-      
-      if (i % 3 === 0) {
-        output += "Fizz";
-      }
-      
-      if (i % 5 === 0) {
-        output += "Buzz";
-      }
-      
-      console.log(output || i);
-    }
+  for (let i = 1; i <= n; i++) {
+    let output = "";
+    
+    if (i % 3 === 0) output += "Fizz";
+    if (i % 5 === 0) output += "Buzz";
+    
+    console.log(output || i);
   }
-  
+}
+
 const N = 100;
-fizzBuzz(N);  
+fizzBuzz(N);
